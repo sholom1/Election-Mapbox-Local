a9db2dfb3d01903730f2f265a59451950fb6410e
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2020-03-10 18:51:50 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict'
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+print("test");
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2hvbG9tMSIsImEiOiJjazdtNXkxb2UwZXAzM2tvbTlzempjcGV1In0.zAVBsEkEYNpTAfw20fw2GA';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
