77340d4a5640fa2034638523e75c9aa5eb996530
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2020-03-14 19:32:59 -0400

    XLSX files with election data now loads correctly

--- app.js
+++ app.js
@@ -25,39 +25,43 @@ var map = new mapboxgl.Map({
       function(response) {
       // Parsing JSON string into object
         districtData = JSON.parse(response);
-        loadXLSX('https://sholom1.github.io/Election-Mapbox-Local/ElectionData.xlsx',
-          function(xlsxfile){
-          var book = xlsx.read(xlsxfile, {type: 'array'})
-          var sheet = book.Sheets[book.SheetNames[0]];
-          var range = xlsx.utils.decode_range(sheet['!ref'])
-          console.log(xlsxfile)
-          });
-        
-        var expression = ['match', ['get', 'elect_dist']];
-        for(feature in districtData.features){
-          districtData.features[feature].properties['color'] = getRandomColor()
-          expression.push(districtData.features[feature].properties.elect_dist, getRandomColor());
-          //console.log(districtData.features[feature])
-        }
-        expression.push('rgba(0,0,0,0)');
-        map.addSource('districts', {
-          'type': 'geojson',
-          'data': 'https://sholom1.github.io/Election-Mapbox-Local/Election%20Districts.geojson',
-          'generateId': true // This ensures that all features have unique IDs
-        });
-        map.addLayer({
-          'id': 'election-district-visualization',
-          'type': 'fill',
-          'source': 'districts',
-          'layout': {},
-          'paint': {
-          'fill-color': expression,
-          'fill-opacity': 0.8
-          }
+        loadXLSX("https://sholom1.github.io/Election-Mapbox-Local/ElectionData.xlsx", 
+          function(worksheet){
+            console.log(worksheet);
+            var range = xlsx.utils.decode_range(worksheet['!ref']);
+            var districtElectionResults = {};
+            for(var row = range.s.r; row < range.e.r; row++){
+              districtElectionResults[joinDistrictNumbers(worksheet['A' + (row + 1).toString()].v.toString(), worksheet['B' + (row + 1).toString()].v.toString())] = "#F7FF00"
+            }
+            var expression = ['match', ['get', 'elect_dist']];
+            for(feature in districtData.features){
+              var featureData = districtData.features[feature];
+              var color = getRandomColor();
+              console.log(featureData.properties.elect_dist);
+              if (featureData.properties.elect_dist in districtElectionResults)
+                color = "#F7FF00"
+              featureData.properties['color'] = color
+              expression.push(featureData.properties.elect_dist, color);
+              //console.log(featureData)
+            }
+            expression.push('rgba(0,0,0,0)');
+            map.addSource('districts', {
+              'type': 'geojson',
+              'data': 'https://sholom1.github.io/Election-Mapbox-Local/Election%20Districts.geojson',
+              'generateId': true // This ensures that all features have unique IDs
+            });
+            map.addLayer({
+              'id': 'election-district-visualization',
+              'type': 'fill',
+              'source': 'districts',
+              'layout': {},
+              'paint': {
+              'fill-color': expression,
+              'fill-opacity': 0.8
+              }
+              });
           });
      });
-    
-    
   })
 function loadJSON(filename, callback) {   
 
@@ -73,15 +77,22 @@ function loadJSON(filename, callback) {
   xobj.send(null);  
 }
 function loadXLSX(filename, callback){
-  var xobj = new XMLHttpRequest();
-  xobj.open('GET', filename, true); 
-  xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-          // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-          callback(xobj.responseText);
-        }
-  };
-  xobj.send(null);  
+  var oReq = new XMLHttpRequest();
+  oReq.open("GET", filename, true);
+  oReq.responseType = "arraybuffer";
+
+  oReq.onload = function(e) {
+    var arraybuffer = oReq.response;
+    /* convert data to binary string */
+    var data = new Uint8Array(arraybuffer);
+    var arr = new Array();
+    for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+    var bstr = arr.join("");
+    /* Call XLSX */
+    var workbook = xlsx.read(bstr, {type:"binary"})
+    callback(workbook.Sheets[workbook.SheetNames[0]]);
+  }
+  oReq.send(null);  
 }
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
@@ -95,5 +106,5 @@ function joinDistrictNumbers(assembly, district){
   while(district.length <= 2){
     district = "0" + district;
   }
-  return assembly + district;
+  return parseInt(assembly + district);
 }
